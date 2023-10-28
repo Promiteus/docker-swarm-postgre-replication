d65ae2609da0c0dfecd17d4608646fabfacc0949
Roman Matveev <r.matveev@socrat.marketing>
2023-10-28 19:20:37 +1000

    28.10.2023 -- Пример репликации СУБД postgres в оркестраторе Docker Swarm. Сценарий init-ru-db.sh - изменено имя мастер сервиса на rep_postgres_master.

--- scripts/cities/prod/init-ru-db.sh
+++ scripts/cities/prod/init-ru-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 name=""
-name=$(docker container ls | awk '{print $NF}' | grep rep_postgres-master)
+name=$(docker container ls | awk '{print $NF}' | grep rep_postgres_master)
 echo $name
 
 docker exec -i $name bash -c "psql -h localhost -d docker_replica -U sa -f /etc/cities/ru/script.sql"
